180477e9ff55e3f9d306db30373128dda071b4d4
agupt039 <2805amanpro@gmail.com>
2022-02-25 10:06:18 +0530

    1 file modified

--- src/app/passenger-dashboard/passenger-dashboard.module.ts
+++ src/app/passenger-dashboard/passenger-dashboard.module.ts
@@ -1,10 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { StoreModule } from '@ngrx/store/public_api';
+import { reducer } from './stores/action-reducer';
 
 @NgModule({
-  imports: [
-    CommonModule
-  ],
-  declarations: []
+  imports: [CommonModule, StoreModule.forFeature('reducer', reducer)],
+  declarations: [],
 })
-export class PassengerDashboardModule { }
+export class PassengerDashboardModule {}
