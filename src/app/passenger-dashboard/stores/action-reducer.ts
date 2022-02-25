f0b0defb42f3ed87c23852f8bbb1ec5e63428405
agupt039 <2805amanpro@gmail.com>
2022-02-25 09:48:36 +0530

    2 files modified

--- src/app/passenger-dashboard/stores/action-reducer.ts
+++ src/app/passenger-dashboard/stores/action-reducer.ts
@@ -1,9 +1,18 @@
+import { ParseSourceFile } from '@angular/compiler/src/parse_util';
 import { createAction } from '@ngrx/store';
+import { ActionReducerMap } from '@ngrx/store/public_api';
 import { union } from '@ngrx/store/src/action_creator';
+import {
+  createFeatureSelector,
+  createSelector,
+} from '@ngrx/store/src/selector';
+import { PassengerState, State } from '../models/passenger.interface';
 
 const loading = createAction('[Passengers] Loading');
 const loaded = createAction('[Passengers] Loaded');
-const data = createAction('[Passengers] data ');
+const data = createAction('[Passengers] data', (data) => ({
+  payload: { data },
+}));
 
 const passengerAction = union({
   loading,
@@ -11,15 +20,48 @@ const passengerAction = union({
   data,
 });
 
+export type PassengerActionInterface = typeof passengerAction;
 
-export type PassengerActionInterface = typeof passengerAction
-
-const initialState = {
+const initialState: PassengerState = {
   loading: false,
   loaded: false,
-  data: []
+  data: [],
+};
+
+function passengerReducer(
+  state = initialState,
+  action: PassengerActionInterface
+) {
+  switch (action.type) {
+    case loading.type: {
+      return { ...state, loading: true };
+    }
+
+    case loaded.type: {
+      return { ...state, loading: false, loaded: true };
+    }
+
+    case data.type: {
+      let data = { ...state.data, ...action.payload.data };
+      return { ...state, data };
+    }
+  }
 }
 
-function passengerReducer(state= intitalState,action:PassengerActionInterface){
+export const reducer: ActionReducerMap<State, PassengerActionInterface> = {
+  Passengers: passengerReducer,
+};
 
-}
+const getPassengers = createFeatureSelector('reducer');
+const getPassengerData = createSelector(
+  getPassengers,
+  (passengers: PassengerState) => passengers.data
+);
+const getLoading = createSelector(
+  getPassengers,
+  (passengers: PassengerState) => passengers.loading
+);
+const getLoaded = createSelector(
+  getPassengers,
+  (passengers: PassengerState) => passengers.loaded
+);
